44dd74b7be6d7146366ca9e8f5e526a509b8cafe
Neville <wee.neville@ymail.com>
2022-05-30 11:52:09 +0800

    log statements

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(p => p.id !== id)
     response.status(204).end()
+    console.log('persons after delete:', persons);
 })
 
 app.post('/api/persons', (request, response) => {
