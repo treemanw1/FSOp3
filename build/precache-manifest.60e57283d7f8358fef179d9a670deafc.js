90e5f9e613982deb705bca3bc8f283dc61a7d973
Neville <wee.neville@ymail.com>
2022-06-01 12:14:40 +0800

    new build

---
+++ build/precache-manifest.60e57283d7f8358fef179d9a670deafc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "252e96a6618b98cbd6a5",
+    "url": "/static/css/main.a74a1594.chunk.css"
+  },
+  {
+    "revision": "252e96a6618b98cbd6a5",
+    "url": "/static/js/main.252e96a6.chunk.js"
+  },
+  {
+    "revision": "bb66abecc51fb2f32f7e",
+    "url": "/static/js/1.bb66abec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3b9b5fe87777e2c607af4eab644878c",
+    "url": "/index.html"
+  }
+];
